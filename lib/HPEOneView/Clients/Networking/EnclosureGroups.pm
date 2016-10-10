226e2989cfc7c150ae68d240ebeecb042c4a4727
ovpt <userzjksls@gmail.com>
2016-10-10 01:15:34 -0400

    get eg

---
+++ lib/HPEOneView/Clients/Networking/EnclosureGroups.pm
@@ -0,0 +1,20 @@
+package HPEOneView::Clients::Networking::EnclosureGroups;
+
+use strict;
+use warnings;
+use JSON::Create 'create_json';
+use JSON::Parse 'parse_json';
+use URI;
+use parent 'HPEOneView::Clients::Security::LoginSessions';
+
+
+my $uri = '/rest/enclosure-groups';
+
+sub get_enclosure_groups {
+    my ($self, %query) = @_;
+    my $_url = URI->new($$self{root_url}.$uri);
+    $_url->query_form(%query);
+    return $self->get($_url);
+}
+
+1;
