bd84e4713b00bc3b990110dd6be0357643212e7f
ovpt <userzjksls@gmail.com>
2016-10-21 08:21:53 -0400

    get storage-systems

---
+++ lib/HPEOneView/Clients/Storage/StorageSystems.pm
@@ -0,0 +1,21 @@
+package HPEOneView::Clients::Storage::StorageSystems;
+
+use strict;
+use warnings;
+use JSON::Create 'create_json';
+use JSON::Parse 'parse_json';
+use URI;
+use parent 'HPEOneView::Clients::Security::LoginSessions';
+
+
+my $uri = '/rest/storage-systems';
+
+sub get_storage_systems {
+    my ($self, %query) = @_;
+    my $_url = URI->new($$self{root_url}.$uri);
+    $_url->query_form(%query);
+    return $self->get($_url);
+}
+
+
+1;
