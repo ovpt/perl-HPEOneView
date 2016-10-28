1c42847f7ddd06d7c501244b4b4248cbec28362d
ovpt <userzjksls@gmail.com>
2016-10-27 22:20:55 -0400

    get storage volume attachments

---
+++ lib/HPEOneView/Clients/Storage/StorageVolumeAttachments.pm
@@ -0,0 +1,21 @@
+package HPEOneView::Clients::Storage::StorageVolumeAttachments;
+
+use strict;
+use warnings;
+use JSON::Create 'create_json';
+use JSON::Parse 'parse_json';
+use URI;
+use parent 'HPEOneView::Clients::Security::LoginSessions';
+
+
+my $uri = '/rest/storage-volume-attachments';
+
+sub get_storage_volume_attachments {
+    my ($self, %query) = @_;
+    my $_url = URI->new($$self{root_url}.$uri);
+    $_url->query_form(%query);
+    return $self->get($_url);
+}
+
+
+1;
