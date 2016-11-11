591f7e40ac3b55d80944e19586ef2c946ff9fcaf
ovpt <userzjksls@gmail.com>
2016-11-11 03:38:42 -0500

    move resource uri to Uris

--- lib/HPEOneView/Clients/Index/Resources.pm
+++ lib/HPEOneView/Clients/Index/Resources.pm
@@ -5,14 +5,13 @@ use warnings;
 use JSON::Create 'create_json';
 use JSON::Parse 'parse_json';
 use URI;
+use HPEOneView::Uris::Index;
 use parent 'HPEOneView::Clients::Security::LoginSessions';
 
 
-my $uri = '/rest/index/resources';
-
 sub get_index_resources {
     my ($self, %query) = @_;
-    my $_url = URI->new($$self{root_url}.$uri);
+    my $_url = URI->new($$self{root_url}.$HPEOneView::Uris::Index::RESOURCES);
     $_url->query_form(%query);
     return $self->get($_url);
 }
---
+++ lib/HPEOneView/Uris/Index.pm
@@ -0,0 +1,11 @@
+package HPEOneView::Uris::Index;
+
+
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our $RESOURCES = '/rest/resources';
